60e8d099ef6feb029302a0dba252968202a26b96
Ououia <jonathansomny@outlook.fr>
2023-08-01 09:18:48 +1100

    Add new controller Dashboard and view

---
+++ app/modules/frontend/controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Phalcon\modules\frontend\controllers;
+
+
+use Phalcon\Models\Composant;
+use Phalcon\Mvc\Controller;
+
+class DashboardController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+
+}
