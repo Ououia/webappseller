6c1089de0db8f025595a266f507de3538d5f78fe
Ououia <jonathansomny@outlook.fr>
2023-08-01 17:22:49 +1100

    Code review with Benoit on method deleteAction() , commented code is mine

--- app/modules/frontend/controllers/EquipeController.php
+++ app/modules/frontend/controllers/EquipeController.php
@@ -20,7 +20,7 @@ class EquipeController extends Controller
 
             $dev = $equipe->getRelated('CompositionEquipe');
 
-            $table .= '<h2>'. $equipe->getName() .'</h2>';
+            $table .= '<h2>'. $equipe->getName() .'<button data-team=' . '"'. $equipe->getId() .'"'.  ' title="Supprimer cette equipe" type="button" class="btn btn-for-delete" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-trash" style="color: #ff0000;"></i></button>' . '</h2>';
             $table .= '<table class="table">';
             $table .= '<thead>';
             $table .= '<tr>';
@@ -46,12 +46,37 @@ class EquipeController extends Controller
             $table .= '</table>';
         }
 
-
-
-
         $this->view->setVar('table', $table);
 
     }
 
+    public function deleteAction()
+    {
+        $team = Team::findFirst($this->request->get("teamid"));
 
+        $error = false;
+        if(!empty($team))
+        {
+            foreach ($team->CompositionEquipe as $teamComposition) {
+                if (!$teamComposition->delete()) {
+                    $error = true;
+                }
+            }
+
+            if (!$error) $team->delete();
+        }
+        return $this->response->redirect("/phalcon/equipe");
+
+//        if($this->request->get("teamid")){
+//            foreach (CompositionEquipe::find('id_team =' .  $this->request->get("teamid")) as $teamCompositionLigne) {
+//                $teamCompositionLigne->delete();
+//            }
+//            if(count(CompositionEquipe::find('id_team =' .  $this->request->get("teamid"))) === 0){
+//               Team::find($this->request->get("teamid"))->delete();
+//            }
+//            return $this->response->redirect("/phalcon/equipe");
+//        } else {
+//            return "Il y a eu une erreur lors de la suppression";
+//        }
+    }
 }
