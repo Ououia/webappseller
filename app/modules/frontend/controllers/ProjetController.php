3bbc1adcd48cb21df44004668f42509dbfe2f3f1
Ououia <jonathansomny@outlook.fr>
2023-09-12 21:52:05 +1100

    Add files for project

---
+++ app/modules/frontend/controllers/ProjetController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Phalcon\modules\frontend\controllers;
+
+
+use mysql_xdevapi\Exception;
+use Phalcon\Models\Chefdeprojet;
+use Phalcon\Models\Composant;
+use Phalcon\Models\CompositionEquipe;
+use Phalcon\Models\Developpeur;
+use Phalcon\Models\Team;
+use \Phalcon\Modules\Frontend\Controllers\ControllerBase;
+
+class ProjetController extends ControllerBase
+{
+    /** Formulaire de création d'equipe */
+    public function indexAction(){}
+
+}
