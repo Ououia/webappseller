9a1e19d824cb1689fca3499c502a3d555eb6b042
Trinkix <jonathan.somny@pacificproweb.nc>
2023-09-29 18:50:32 +1100

    feat: add/remove member from team added

--- app/modules/frontend/controllers/EquipeController.php
+++ app/modules/frontend/controllers/EquipeController.php
@@ -24,13 +24,16 @@ class EquipeController extends ControllerBase
 
             $dev = $equipe->getRelated('CompositionEquipe');
 
-            $table .= '<h2>' . $equipe->getName() . '<button data-delete-team=' . '"' . $equipe->getId() . '"' . ' title="Supprimer cette equipe" type="button" class="btn btn-for-delete" data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="fa-solid fa-trash fa-xl" style="color: #ff0000;"></i></button>' . '</h2>';
+            $table .= '<h2 class="text-capitalize">' . $equipe->getName() . '<button data-action="delete-team" data-delete =' . '"' . $equipe->getId() . '"' . ' title="Supprimer cette equipe" type="button" class="btn btn-for-delete" data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="fa-solid fa-trash fa-xl" style="color: #ff0000;"></i></button>' . '</h2>';
+            $table .= '<button data-action="add-dev"   data-cdp =' . '"' . $equipe->Chefdeprojet->getId() . '"' . 'data-team =' . '"' . $equipe->getId() . '"' . 'class="button-for-modify fw-bold btn btn-info text-white" data-bs-toggle="modal" data-bs-target="#modifyModal">Ajouter membre equipe</button>';
             $table .= '<table class="table">';
             $table .= '<thead>';
             $table .= '<tr>';
             $table .= '<th>Role</th>';
             $table .= '<th>Nom</th>';
             $table .= '<th>Niveau Competence</th>';
+            $table .= '<th>Boost(%)/Indice de production </th>';
+            $table .= '<th>Action</th>';
             $table .= '</tr>';
             $table .= '</thead>';
             $table .= '<tbody>';
@@ -38,13 +41,16 @@ class EquipeController extends ControllerBase
             $table .= '<td>Chef de projet <i class="fa-solid fa-crown" style="color: #d3d600;"></i></td>';
             $table .= '<td>' . $equipe->Chefdeprojet->Collaborateur->getPrenomNom() . '</td>';
             $table .= '<td>' . $equipe->Chefdeprojet->Collaborateur->getCompetenceLibele() . '</td>';
+            $table .= '<td>' . $equipe->Chefdeprojet->getBoostProduction() . '%</td>';
+            $table .= '<td></td>';
             $table .= '</tr>';
             foreach ($dev as $d) {
                 $table .= '<tr>';
                 $table .= '<td> Developpeur ' . $d->Developpeur->enumNivCompetence() . '</td>';
                 $table .= '<td>' . $d->Developpeur->Collaborateur->getPrenomNom() . '</td>';
                 $table .= '<td>' . $d->Developpeur->Collaborateur->getCompetenceLibele() . '</td>';
-                $table .= '<td><button  data-compoid =' . '"' . $d->getId() . '"' . '  data-cdp =' . '"' . $equipe->Chefdeprojet->getId() . '"' . 'data-team =' . '"' . $equipe->getId() . '"' . '  class="button-for-modify btn btn-info text-white" data-bs-toggle="modal" data-bs-target="#modifyModal">Modifier</button></td>';
+                $table .= '<td>' . $d->Developpeur->getIndiceProduction() . '</td>';
+                $table .= '<td><button data-action="modify-dev" data-compoid =' . '"' . $d->getId() . '"' . '  data-cdp =' . '"' . $equipe->Chefdeprojet->getId() . '"' . 'data-team =' . '"' . $equipe->getId() . '"' . 'class="button-for-modify fw-bold btn btn-info text-white" data-bs-toggle="modal" data-bs-target="#modifyModal">Modifier</button> <button data-action="delete-compoid" data-delete =' . '"' . $d->getId() . '"' . '  type="button" class="btn fw-bold btn-for-delete btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">Supprimer</button></td>';
                 $table .= '</tr>';
             }
             $table .= '</tbody>';
@@ -55,6 +61,67 @@ class EquipeController extends ControllerBase
     }
 
 
+    /** Permet d'ajouter un developpeur dans une equipe donnée */
+    public function addDevInTeamAction()
+    {
+        if ($this->request->isPost()) {
+            $referer = $this->request->getHTTPReferer();
+
+            $newDev = $this->request->get('newDevName', "int");
+            $teamId = $this->request->get('compoId', "int");
+
+            if ($newDev && $teamId) {
+
+                $teams = CompositionEquipe::getDevsByTeamIds($teamId);
+
+                if (count($teams) >= 3) {
+                    $this->flashSession->error("Cette equipe est deja pleine");
+                    return $this->response->redirect($referer);
+                }
+
+                if (!Developpeur::findFirst($newDev)) {
+                    $this->flashSession->error("Ce developpeur n'existe pas");
+                    return $this->response->redirect($referer);
+                }
+
+                if (!Team::findFirst($teamId)) {
+                    $this->flashSession->error("Cette equipe n'existe pas");
+                    return $this->response->redirect($referer);
+                }
+
+                $existingEntry = CompositionEquipe::findFirst([
+                    'conditions' => 'id_team = ?1 AND id_dev = ?2',
+                    'bind' => [
+                        1 => $teamId,
+                        2 => $newDev
+                    ]
+                ]);
+
+                if (!$existingEntry) {
+                    $dev = (new CompositionEquipe())
+                        ->setIdTeam($teamId)
+                        ->setIdDev($newDev);
+
+                    if ($dev->save()) {
+                        $this->flashSession->success("Developpeur rajouter a l'equipe.");
+                        return $this->response->redirect($referer);
+                    } else {
+                        $this->flashSession->error("Error while adding dev to the team.");
+                        return $this->response->redirect($referer);
+                    }
+                } else {
+                    $this->flashSession->error("This dev is already part of the team.");
+                    return $this->response->redirect($referer);
+                }
+            } else {
+                $this->flashSession->error("Il y a eu une erreur, veuillez reesayer");
+                return $this->response->redirect($referer);
+            }
+        }
+    }
+
+
+    /** Permet de recuperer les developpeur dans une equipe donnée */
     public function modifyDevInTeamAction(): \Phalcon\Http\ResponseInterface
     {
         if ($this->request->isPost()) {
@@ -95,7 +162,7 @@ class EquipeController extends ControllerBase
         }
     }
 
-    /** Permet de modifier un developpeur dans une equipe donnée */
+    /** Permet de recuperer les developpeur disponible pour une equipe avec un chef de projet donné*/
     public function getAvailableDevsAction(): \Phalcon\Http\ResponseInterface
     {
         $response = new Response();
@@ -150,9 +217,9 @@ class EquipeController extends ControllerBase
 
 
     /** Permet de supprimer une equipe par rapport a son id */
-    public function deleteAction(): \Phalcon\Http\ResponseInterface
+    public function deleteTeamAction(): \Phalcon\Http\ResponseInterface
     {
-        $team = Team::findFirst($this->request->get("teamid"));
+        $team = Team::findFirst($this->request->get("teamid", "int"));
 
         $error = false;
         if (!empty($team)) {
@@ -167,4 +234,30 @@ class EquipeController extends ControllerBase
         }
         return $this->response->redirect($this->url->get("/equipe"));
     }
+
+
+
+    public function deleteDevFromTeamAction(): \Phalcon\Http\ResponseInterface
+    {
+        if ($this->request->isGet()) {
+            $referer = $this->request->getHTTPReferer();
+
+            $dev = CompositionEquipe::findFirst($this->request->get("devid", "int"));
+
+            if (count($dev) === 0) {
+                $this->flashSession->error("Il y a eu une erreur, veuillez reesayer");
+                return $this->response->redirect($referer);
+            }
+
+            if ($dev->delete()) {
+                $this->flashSession->success("Developpeur supprimer avec succès");
+                return $this->response->redirect($referer);
+            }
+
+        } else {
+            return $this->flashSession->error("La requête n'est pas de type Get");
+        }
+    }
+
+
 }
