6e2f97f5ea1b2e30a591cc06543ab94978d27399
Trinkix <jonathan.somny@pacificproweb.nc>
2023-09-29 20:09:51 +1100

    feat : add local exemple

---
+++ app/config/local-exemple.php
@@ -0,0 +1,13 @@
+<?php
+
+$localConfig = [
+    "database" => [
+        'adapter' => 'Mysql',
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'dbname' => '',
+        'charset' => 'utf8',
+    ],
+    "baseUri" => ""
+];
